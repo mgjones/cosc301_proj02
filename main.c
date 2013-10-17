5e094d0ca4a4a6c7da9e640bf6509d35355dfa00
nolan <nolan@nolan-Lenovo-G570.(none)>
2013-10-16 21:01:42 -0400

    added tokenify

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <strings.h>
 #include <ctype.h>
 #include <sys/resource.h>
 #include <sys/wait.h>
@@ -11,10 +12,30 @@
 #include <poll.h>
 #include <signal.h>
 
+char **tokenify(const char *str) {
+
+	const char *sep = ";";
+	char *temp, *c;
+	char *s = strdup(str);
+
+	char **words = malloc(strlen(s) * sizeof(char));
+
+	int count = 0;
+
+	for (c = strtok_r(s, sep, &temp); c != NULL; c = strtok_r(NULL, sep, &temp)){
+		words[count] = strdup(c);
+		count++;
+	}
+
+	words[count] = NULL;
+	free(s);
+	
+	return words;
+}
 
 int main(int argc, char **argv) {
+	char* prompt = "mjng$ ";
 
-    printf("main.c, so lonely.  no code here yet :-(\n");
 	printf ("%s", prompt );
 	fflush ( stdout ); /* if you want the prompt to immediately appear ,
 						call fflush . it ’flushes ’ the output to screen */
@@ -22,6 +43,7 @@ int main(int argc, char **argv) {
 	char buffer [1024];
 	while ( fgets(buffer , 1024 , stdin) != NULL) {
 	// process current command line in buffer
+		
 
 	
 	}
