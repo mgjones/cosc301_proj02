003d1c6ccd0672d92ab93544ffbcaa3fd2d1050f
nolan <nolan@nolan-Lenovo-G570.(none)>
2013-10-17 21:22:30 -0400

    added sequential and check_mode function, check_mode not working

--- main.c
+++ main.c
@@ -41,20 +41,61 @@ char **tokenify(const char *str,int swap) {
 	return words;
 }
 
-void exit(const char *str){
-	// not sure if this makes sense
-
+int check_mode(char **line, int mode_choice){
+	
+	char** command = tokenify(line[0],0);
+	if (strncmp(command[0],"mode",4) == 0) {
+			if (command[1] == NULL){
+				if (mode_choice == 0) {
+					printf("mode: Sequential\n");
+				} else {
+					printf("mode: Parallel\n");
+				}
+			} else {
+				//there is a parameter
+				if (strncmp(command[1], "sequential", 10) == 0 || strncmp(command[1], "s", 1) == 0){
+					mode_choice = 0;
+				} else if (strncmp(command[1], "parallel", 10) == 0 || strncmp(command[1], "p", 1) == 0){
+					mode_choice = 1;				
+				}
+			}// end else
+	}// end first if
+	return mode_choice;
 }
 
 
 
+void sequential(char **line) {
 
+	pid_t child;
+	int i = 0;
+	while(line[i] != NULL){
+		char** command = tokenify(line[i],0); // parses a command			
+		child = fork();
 
+		if (child == 0){
+			// this is child process
+			execv(command[0],command);
+		} else {
+			// this is parent process
+			int *status = NULL;
+			waitpid(child,status,0);
+		}
+		i++;
+	}
+	return;
+}
 
+void parallel(char **line) {
+	// code goes here
+	printf("we're inside god's kangaroo pouch\n");
+	return;
+}
 
 
 int main(int argc, char **argv) {
 	char* prompt = "mjng$ ";
+	int mode_choice = 0;
 
 	printf ("%s", prompt );
 	fflush ( stdout ); /* if you want the prompt to immediately appear ,
@@ -66,14 +107,16 @@ int main(int argc, char **argv) {
 
 		char** line = tokenify(buffer,3); //cleans all comments out
 		line = tokenify(line[0],1); // parses line into commands
-
-		int i = 0;
-		while(line[i] != NULL){
-			char** command = tokenify(line[i],0); // parses a command
-			// call on execv
-			i++;
+		mode_choice = check_mode(line, mode_choice);
+		
+		// choose mode to run processes
+		if (mode_choice == 0){
+			sequential(line);		
+		} else {
+			parallel(line);	
 		}
 
+		printf ("%s", prompt );
 
 
 	}
